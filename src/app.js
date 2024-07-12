b1aca05e1e6309bf6804c83d5e8aa890645fb44f
David Alonso <125155276+DavidAlonsoRod@users.noreply.github.com>
2024-07-12 15:42:07 +0000

    Entrega generador de Excusas por DAvid Alonso

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  function generarExcusa() {
+    let who = ["mi novio", "Pablo", "Pabluchi", "mi amor"];
+    let action = [
+      "ha apetecido",
+      "quiere que le cocine",
+      "me ha sugerido para comer",
+      "le gustaría comer"
+    ];
+    let what = [
+      "pechuga de pavo al curry rojo",
+      "calamares en su tinta",
+      "ragout de pato a la naranja",
+      "unos callos con garbanzos"
+    ];
+    let when = [
+      "Esta tarde",
+      "Hoy",
+      "Cuando nos hemos despertado",
+      "Durante el desayuno",
+      "Cuando nos hemos visto"
+    ];
+
+    let sujeto = who[Math.floor(Math.random() * who.length)];
+    let accion = action[Math.floor(Math.random() * action.length)];
+    let que = what[Math.floor(Math.random() * what.length)];
+    let cuando = when[Math.floor(Math.random() * when.length)];
+
+    return `${cuando} ${sujeto} ${accion} ${que} .`;
+  }
+
+  document.getElementById("excusa").innerText = generarExcusa();
+  document.getElementById("botonExcusa").addEventListener("click", function() {
+    document.getElementById("excusa").innerText = generarExcusa();
+  });
   console.log("Hello Rigo from the console!");
 };
